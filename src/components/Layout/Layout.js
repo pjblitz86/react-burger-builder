264ad860906700cb68a8e8bfc03289a4011643ba
Paulius Juknis <paulius.juknis@gmail.com>
2018-12-13 13:34:12 +0200

    add SideDrawer component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from "react";
 import Hoc from "../../hoc/Hoc";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = props => (
   <Hoc>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Hoc>
 );
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,7 @@ const navigationItems = () => (
     <NavigationItem link="/" active>
       Burger Builder
     </NavigationItem>
-    <NavigationItem link="/">Chekout</NavigationItem>
+    <NavigationItem link="/">Checkout</NavigationItem>
   </ul>
 );
 
