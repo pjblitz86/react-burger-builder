acfd9e7f9f018f25f71113eee08679de364137b9
Paulius Juknis <paulius.juknis@gmail.com>
2018-12-17 11:53:54 +0200

    improving performance by not updating oderSummary if modal doesnt update

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import Hoc from "../../../hoc/Hoc";
 import Button from "../../UI/Button/Button";
 
-const oderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+class OderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+          {this.props.ingredients[igKey]}
+        </li>
+      );
+    });
     return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
+      <Hoc>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Hoc>
     );
-  });
-  return (
-    <Hoc>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Hoc>
-  );
-};
+  }
+}
 
-export default oderSummary;
+export default OderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Modal.css";
 import Hoc from "../../../hoc/Hoc";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = props => (
-  <Hoc>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0"
-      }}
-    >
-      {props.children}
-    </div>
-  </Hoc>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  componentWillUpdate() {
+    console.log("Modal will update");
+  }
+
+  render() {
+    return (
+      <Hoc>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0"
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Hoc>
+    );
+  }
+}
+
+export default Modal;
